3a3ab783c5e0dbea5ae7c7e49f210d7cca5ef18b
camilo1124 <ccardenasv@unal.edu.co>
2024-05-05 10:35:47 -0500

    Code nessesary to add new branches

--- main.tf
+++ main.tf
@@ -1,7 +1,20 @@
 resource "github_repository" "TF_Lab_repo" {
-  name        = "Lab Archi & Infra"
+  name        = "LabArchiAndInfra"
   description = "Laboratorio Infraestructura como codigo"
 
   visibility = "public"
 
+}
+
+resource "github_branch" "development" {
+  repository = "LabArchiAndInfra"
+  branch     = "development"
+}
+resource "github_branch" "lab2" {
+  repository = "LabArchiAndInfra"
+  branch     = "lab2"
+}
+resource "github_branch" "lab3" {
+  repository = "LabArchiAndInfra"
+  branch     = "lab3"
 }
